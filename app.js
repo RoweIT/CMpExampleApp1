f1ff3f59a8f86daf0d7f4eca243d9017d77cd5f3
Gary Ratcliffe <gary@roweit.co.uk>
2019-06-27 16:16:02 +0100

    Bring up todate with CCS version.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var AppConfig = require('ccsexamplenpmmodule');
 
 
 var indexRouter = require('./routes/index');
+var awsRouter = require('./routes/aws');
+var redisRouter = require('./routes/redis');
 
 var app = express();
 
@@ -37,6 +39,8 @@ app.use('/assets', express.static(path.join(__dirname, 'node_modules/govuk-front
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
+app.use('/aws', awsRouter);
+app.use('/redis', redisRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
